53ff70b3b2c2a965bb2e8ea55a72db3202183d37
Oliver Hole Ekelund <oliver.hole.ekelund@hotmail.no>
2024-07-08 15:48:19 +0200

    found y_max and colored them out

---
+++ check_versions.py
@@ -0,0 +1,6 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("NumPy version:", np.__version__)
+print("Matplotlib version:", matplotlib.__version__)
--- ground_det.py
+++ ground_det.py
@@ -1,16 +1,15 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import open3d as o3d
 import imageio.v3 as iio
 
 # Read point cloud:
-pcd = o3d.io.read_point_cloud("data/depth_3.ply")
+pcd = o3d.io.read_point_cloud("data/Road.ply")
 # Create a 3D coordinate system:
 origin = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.5)
 # geometries to draw:
 geometries = [pcd, origin]
-# Visualize:
-#o3d.visualization.draw_geometries(geometries)
 
 # Get max and min points of each axis x, y and z:
 x_max = max(pcd.points, key=lambda x: x[0])
@@ -20,18 +19,6 @@ x_min = min(pcd.points, key=lambda x: x[0])
 y_min = min(pcd.points, key=lambda x: x[1])
 z_min = min(pcd.points, key=lambda x: x[2])
 
-# Visualize with axis. Blue, red, green arrow represent Z-axis, X-axis, Y-axis.
-"""
-viewer = o3d.visualization.Visualizer()
-viewer.create_window()
-for geometry in geometries:
-    viewer.add_geometry(geometry)
-opt = viewer.get_render_option()
-opt.show_coordinate_frame = True
-opt.background_color = np.asarray([0.5, 0.5, 0.5])
-viewer.run()
-viewer.destroy_window()
-"""
 
 # Colors:
 RED = [1., 0., 0.]
@@ -40,6 +27,7 @@ BLUE = [0., 0., 1.]
 YELLOW = [1., 1., 0.]
 MAGENTA = [1., 0., 1.]
 CYAN = [0., 1., 1.]
+GRAY = [0.5, 0.5, 0.5]
 
 positions = [x_max, y_max, z_max, x_min, y_min, z_min]
 colors = [RED, GREEN, BLUE, MAGENTA, YELLOW, CYAN]
@@ -55,5 +43,26 @@ for i in range(len(positions)):
    # add to geometry list to display later:
    geometries.append(sphere)
 
+# Define a threshold:
+THRESHOLD = 0.075
+
+# Get the max value along the y-axis:
+y_max = max(pcd.points, key=lambda x: x[1])[1]
+
+# Get the original points color to be updated:
+# pcd_colors = np.asarray(pcd.colors)
+pcd_colors = np.full((len(pcd.points), 3), GRAY)
+
+# Number of points:
+n_points = pcd_colors.shape[0]
+
+# update color:
+for i in range(n_points):
+    # if the current point is aground point:
+    if pcd.points[i][1] >= y_max - THRESHOLD:
+        pcd_colors[i] = GREEN  # color it green
+
+pcd.colors = o3d.utility.Vector3dVector(pcd_colors)
+
 # Display:
-o3d.visualization.draw_geometries(geometries)
+o3d.visualization.draw_geometries([pcd, origin])
